a9f1a5f8c407384737fa0e8c038c1d00bf7ca891
Thomas Ridings <Mail@thomasridings.com>
2018-03-07 20:26:14 +0000

    memsey demo

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ app.use('/assets', express.static(path.join(__dirname, '/assets')));
 app.use('/images', express.static(path.join(__dirname, '/images')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+app.get('/memsey', (req, res) => res.send({"hey man"}));
 
-app.listen(3300, () => console.log('Example app listening on port 3000!'))
+var port = 3300;
+
+app.listen(port, () => console.log('Example app listening on port ' + port));
